d5ea0e32d32d87749a68539d831c496e9ad0bc15
swiggi5 <barantuna5454@gmail.com>
2023-02-02 17:59:47 +0300

    ...
    https://laracasts.com/series/laravel-8-from-scratch/episodes/34

--- example-app/resources/views/components/layout.blade.php
+++ example-app/resources/views/components/layout.blade.php
@@ -10,21 +10,21 @@
     <nav class="md:flex md:justify-between md:items-center">
         <div>
             <a href="/">
-                <img src="./images/logo.svg" alt="Laracasts Logo" width="165" height="16">
+                <img src="/images/logo.svg" alt="Laracasts Logo" width="165" height="16">
             </a>
         </div>
 
         <div class="mt-8 md:mt-0">
             <a href="/" class="text-xs font-bold uppercase">Home Page</a>
 
-            <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
+            <a href="/" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
                 Subscribe for Updates
             </a>
         </div>
     </nav>
 {{$slot}}
     <footer class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
-        <img src="./images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
+        <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
         <h5 class="text-3xl">Stay in touch with the latest posts</h5>
         <p class="text-sm mt-3">Promise to keep the inbox clean. No bugs.</p>
 
